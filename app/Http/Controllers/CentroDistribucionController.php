c6c96e0ac133cd623836b2c773c062485c9b9586
Elinksnow <elizabeth.elcl@gmail.com>
2023-01-15 15:45:26 -0300

    inicio proyecto

---
+++ app/Http/Controllers/CentroDistribucionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\centro_distribucion;
+use Illuminate\Http\Request;
+
+class CentroDistribucionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\centro_distribucion  $centro_distribucion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(centro_distribucion $centro_distribucion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\centro_distribucion  $centro_distribucion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(centro_distribucion $centro_distribucion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\centro_distribucion  $centro_distribucion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, centro_distribucion $centro_distribucion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\centro_distribucion  $centro_distribucion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(centro_distribucion $centro_distribucion)
+    {
+        //
+    }
+}
